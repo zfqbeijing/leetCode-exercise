583849eb9f4cabef5357519fa108abd7a742e70c
Administrator <it_zfq@163.com>
2020-09-04 19:44:42 +0800

    leetCode 算法：	3.无重复字符的最长子串

---
+++ src/cn/com/zfq/arithmetic/linkedList/TwoNumberAdd.java
@@ -0,0 +1,256 @@
+package cn.com.zfq.arithmetic.linkedList;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/***
+ *
+ * @Author : 张枫琴
+ * @Description
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储一位数字。
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ * 示例：
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/add-two-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Version : v1.0.0
+ * @Date : Created in 2020/8/30 17:04
+ * @Modified By : 
+ **/
+public class TwoNumberAdd {
+
+    /**
+     * 对两个链表进行遍历按照位数进行相加
+     * <p>
+     * 执行用时：2 ms, 在所有 Java 提交中击败了99.89%的用户
+     * 内存消耗：39.8 MB, 在所有 Java 提交中击败了59.80%的用户
+     *
+     * @param l1 第一个链表
+     * @param l2 第二个链表
+     * @return
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // 定义一个新链表 用来存储计算结果，默认创建的是带头节点所以默认是0
+        ListNode temp = new ListNode(0);
+        // 记住第一个节点
+        ListNode node = temp;
+        // 进位 也就是 如果相加的是大于10 我们在下一位的时候加1，默认是0
+        int carry = 0;
+        // 对两个两个链表进行遍历
+        while (l1 != null || l2 != null) {
+            // 如果l1链表不为null 就取出l1的值，否则为0
+            int x = (l1 != null) ? l1.val : 0;
+            // 如果l2链表不为null 就取出l2的值，否则为0
+            int y = (l2 != null) ? l2.val : 0;
+            // 因为都是个位数相加 所以最大指是等于19的 ，得把进位加上
+            int sum = x + y + carry;
+            // 把得出的结果存入链表中，但是必须跟10取余 只要不管是否大于等于10 都会取得个位数的
+            temp.next = new ListNode(sum % 10);
+            // 指向下一个节点
+            temp = temp.next;
+            // 求进位 如果大于10 即可以求出进位为1 否则就是0
+            carry = sum / 10;
+            // 判断l1链表是否为空 如果不为空则指向下一个节点
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            // 判断l2链表是否为空 如果不为空则指向下一个节点
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        // 有可能存在一种情况 但两个链表最后的值计算 大于等于10 的情况且两个链表都为空了 ，得把进位放入最后一个节点
+        if (carry > 0) {
+            temp.next = new ListNode(carry);
+            temp = temp.next;
+        }
+        // 因为默认创建的是带头节点 所以我们要从temp链表的下一个开始
+        return node.next;
+    }
+
+    /**
+     * 如果链表中的数字不是按逆序存储的呢？例如：
+     * (3→4→2)+(4→6→5)=8→0→7
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbersExt(ListNode l1, ListNode l2) {
+        // 定义一个新链表 用来存储计算结果，默认创建的是带头节点所以默认是0
+        ListNode temp = new ListNode(0);
+        // 记住第一个节点
+        ListNode node = temp;
+        // 进位 也就是 如果相加的是大于10 我们在下一位的时候加1，默认是0
+        int carry = 0;
+        // 使用栈 接收l1链表的数据
+        Stack<Integer> stackL1 = new Stack<>();
+        // 使用栈 接收l2链表的数据
+        Stack<Integer> stackL2 = new Stack<>();
+        // 对两个两个链表进行遍历,并且加入栈中
+        while (l1 != null || l2 != null) {
+            // 判断l1链表是否为空 如果不为空则指向下一个节点
+            if (l1 != null) {
+                stackL1.push(l1.val);
+                l1 = l1.next;
+            }
+            // 判断l2链表是否为空 如果不为空则指向下一个节点
+            if (l2 != null) {
+                stackL2.push(l2.val);
+                l2 = l2.next;
+            }
+        }
+        // 遍历两个栈
+        while (!stackL1.empty() || !stackL2.empty()) {
+            // 如果stackL1栈不为null 就弹栈，否则为0
+            int x = (!stackL1.empty()) ? stackL1.pop() : 0;
+            // 如果stackL2栈不为null 就弹栈，否则为0
+            int y = (!stackL2.empty()) ? stackL2.pop() : 0;
+            // 因为都是个位数相加 所以最大指是等于19的 ，得把进位加上
+            int sum = x + y + carry;
+            // 把得出的结果存入链表中，但是必须跟10取余 只要不管是否大于等于10 都会取得个位数的
+            temp.next = new ListNode(sum % 10);
+            // 指向下一个节点
+            temp = temp.next;
+            // 求进位 如果大于10 即可以求出进位为1 否则就是0
+            carry = sum / 10;
+        }
+        // 有可能存在一种情况 但两个链表最后的值计算 大于等于10 的情况且两个链表都为空了 ，得把进位放入最后一个节点
+        if (carry > 0) {
+            temp.next = new ListNode(carry);
+            temp = temp.next;
+        }
+        // 因为默认创建的是带头节点 所以我们要从temp链表的下一个开始
+        return node.next;
+    }
+
+    /**
+     * 测试打印类
+     *
+     * @param node
+     */
+    public void print(ListNode node) {
+        while (true) {
+            if (node.next == null) {
+                System.out.print(node.val);
+                break;
+            }
+            System.out.print(node.val + " -> ");
+            node = node.next;
+        }
+    }
+
+    public void printTemplate(ListNode l1, ListNode l2) {
+        System.out.print("(");
+        print(l1);
+        System.out.print(") + (");
+        print(l2);
+        System.out.print(") 计算结果:");
+        System.out.println();
+    }
+
+    /**
+     * 做测试数据
+     */
+    public List<ListNode> testManufacturingData() {
+        List<ListNode> list = new ArrayList<>();
+
+        /**
+         * 测试用例1：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+         * 预测结果：7 -> 0 -> 8
+         */
+        ListNode test1_l1 = new ListNode(2);
+        test1_l1.next = new ListNode(4);
+        test1_l1.next.next = new ListNode(3);
+        ListNode test1_l2 = new ListNode(5);
+        test1_l2.next = new ListNode(6);
+        test1_l2.next.next = new ListNode(4);
+        list.add(test1_l1);
+        list.add(test1_l2);
+        /**
+         * 测试用例2：(9 -> 3 -> 5) + (8 -> 5 -> 4 -> 1)
+         * 预测结果：7 -> 9 -> 9 -> 1
+         */
+        ListNode test2_l1 = new ListNode(9);
+        test2_l1.next = new ListNode(3);
+        test2_l1.next.next = new ListNode(5);
+        ListNode test2_l2 = new ListNode(8);
+        test2_l2.next = new ListNode(5);
+        test2_l2.next.next = new ListNode(4);
+        test2_l2.next.next.next = new ListNode(1);
+
+        list.add(test2_l1);
+        list.add(test2_l2);
+        /**
+         * 测试用例3：(6 -> 2 -> 8 -> 7) + (5 -> 6 -> 1)
+         * 预测结果：1 -> 9 -> 9 -> 7
+         */
+        ListNode test3_l1 = new ListNode(6);
+        test3_l1.next = new ListNode(2);
+        test3_l1.next.next = new ListNode(8);
+        test3_l1.next.next.next = new ListNode(7);
+        ListNode test3_l2 = new ListNode(5);
+        test3_l2.next = new ListNode(6);
+        test3_l2.next.next = new ListNode(1);
+        list.add(test3_l1);
+        list.add(test3_l2);
+        /**
+         * 测试用例4：(1 -> 1 -> 6 ) + (3 -> 0 -> 4)
+         * 预测结果：4 -> 2 -> 0 -> 1
+         */
+        ListNode test4_l1 = new ListNode(1);
+        test4_l1.next = new ListNode(1);
+        test4_l1.next.next = new ListNode(6);
+        ListNode test4_l2 = new ListNode(3);
+        test4_l2.next = new ListNode(1);
+        test4_l2.next.next = new ListNode(4);
+        list.add(test4_l1);
+        list.add(test4_l2);
+        return list;
+    }
+
+    /**
+     * 计算
+     */
+    public void calculate() {
+        List<ListNode> list = testManufacturingData();
+        // 测试 addTwoNumbers
+        for (int i = 0; i < (list.size()) / 2; i++) {
+            printTemplate(list.get(i), list.get(i + 1));
+            print(addTwoNumbers(list.get(i), list.get(i+1)));
+            System.out.println();
+        }
+
+        for (int i = 0; i < (list.size()) / 2; i++) {
+            printTemplate(list.get(i), list.get(i + 1));
+            print(addTwoNumbersExt(list.get(i), list.get(i + 1)));
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        TwoNumberAdd add = new TwoNumberAdd();
+        add.calculate();
+    }
+
+}
+
+/**
+ * 定义一个链表
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        this.val = x;
+    }
+}
---
+++ src/cn/com/zfq/arithmetic/strings/LengthSubstringNotRepetition.java
@@ -0,0 +1,115 @@
+package cn.com.zfq.arithmetic.strings;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/***
+ * @ClassName: LengthSubstringNotRepetition
+ * @Description:
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ * 示例 1:
+ *
+ * 输入: "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 示例 2:
+ *
+ * 输入: "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 示例 3:
+ *
+ * 输入: "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ *     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author: 张枫琴
+ * @Date: 2020/9/4 18:53
+ * @Version: v1.0
+ * @Modified By:
+ *
+ */
+public class LengthSubstringNotRepetition {
+
+    public static void main(String[] args) {
+        LengthSubstringNotRepetition l = new LengthSubstringNotRepetition();
+//        System.out.println("abcabcbb: " + l.lengthOfLongestSubstring("abcabcbb"));
+//        System.out.println("bbbbb: " + l.lengthOfLongestSubstring("bbbbb"));
+//        System.out.println("pwwkew: " + l.lengthOfLongestSubstring("pwwkew"));
+
+        System.out.println("abcabcbb: " + l.lengthOfLongestSubstring2("abcabcbb"));
+        System.out.println("bbbbb: " + l.lengthOfLongestSubstring2("bbbbb"));
+        System.out.println("pwwkew: " + l.lengthOfLongestSubstring2("pwwkew"));
+
+    }
+
+    /**
+     * 执行用时：8 ms, 在所有 Java 提交中击败了69.67%的用户
+     * 内存消耗：39.8 MB, 在所有 Java 提交中击败了77.26%的用户
+     *
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring(String s) {
+        // 记录每一个字符是否重复过
+        Set<Character> set = new HashSet<>();
+        // 指针 表示向右移动的距离,表示距左边的距离
+        int r = -1;
+        // 字符串的长度
+        int n = s.length();
+        // 记录最长的
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            // 当左指针不等于0的时候移除最左边的字符串
+            if (i != 0) {
+                set.remove(s.charAt(i - 1));
+            }
+            // 窗口进行滑动
+            while (r + 1 < n && !set.contains(s.charAt(r + 1))) {
+                // 指针不断的向右滑动，不断的添加
+                set.add(s.charAt(r + 1));
+                r++;
+            }
+            // 这是从i到r个字符串中最长的无重复的子串
+            max = Math.max(max, r - i + 1);
+        }
+
+        return max;
+    }
+
+    /**
+     * 执行用时：8 ms, 在所有 Java 提交中击败了69.67%的用户
+     * 内存消耗：39.9 MB, 在所有 Java 提交中击败了71.15%的用户
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring2(String s) {
+        if (s == null || s.length() <= 0) {
+            return 0;
+        }
+
+        // 记录每一个元素，且该元素的目前的位置
+        Map<Character, Integer> map = new HashMap<>(16);
+        // 指针 表示距左边的指针
+        int l = 0;
+        // 记录最长的
+        int max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            // 判断该元素在哈希表中是否存在，如果存在比较当前的和以前的谁大
+            if (map.containsKey(s.charAt(i))){
+               l = Math.max(l,map.get(s.charAt(i))+1);
+            }
+            // 添加到哈希表中
+            map.put(s.charAt(i),i);
+            // 计算当前窗口里面的 最大值和之前最大比较
+            max = Math.max(max,i-l+1);
+        }
+        return max;
+    }
+}
